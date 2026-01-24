4a46ed2f52a51445dac2f2c0a48dd4fe3ba0e5f3
lbarto12 <lbarto12@hotmail.com>
2026-01-24 16:47:03 -0500

    session handlers :)

--- server/handlers/sessionhandlers/basic.go
+++ server/handlers/sessionhandlers/basic.go
@@ -1,15 +1,92 @@
 package sessionhandlers
 
-import "net/http"
+import (
+	"net/http"
+	"server/api/apirequests"
+	"server/api/apiresponses"
+	"server/api/tools/internaltools/passwords"
+	"server/api/tools/internaltools/webtokens"
+	"server/handlers/generated/gensessions"
+	"server/sqlc/sqlgen"
+
+	"github.com/labstack/gommon/log"
+)
 
 // Signs a user in.
 // (POST /signin)
 func (handler Handler) SignIn(w http.ResponseWriter, r *http.Request) {
+	request, err := apirequests.Request[gensessions.SessionItem](r)
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+
+	qtx := sqlgen.New(handler.Services.Postgres)
+
+	user, err := qtx.GetUserAccountByEmail(r.Context(), string(request.Email))
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := passwords.CompareHashAndPassword(string(request.Password), user.PasswordHash); err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	token, _, err := webtokens.GenerateJWT(user.ID)
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	apiresponses.Success(w, gensessions.SessionResponse{
+		Token: token,
+	})
 
 }
 
 // Registers a new user.
 // (POST /signup)
 func (handler Handler) SignUp(w http.ResponseWriter, r *http.Request) {
+	request, err := apirequests.Request[gensessions.SessionItem](r)
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
 
+	qtx := sqlgen.New(handler.Services.Postgres)
+
+	pswd, err := passwords.GenerateFromPassword(request.Password, passwords.NewDefaultPasswordGenerationOptions())
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	user, err := qtx.CreateAccount(r.Context(), sqlgen.CreateAccountParams{
+		UserEmail:    string(request.Email),
+		PasswordHash: pswd,
+	})
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	token, _, err := webtokens.GenerateJWT(user.ID)
+	if err != nil {
+		log.Error(err)
+		apiresponses.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+
+	apiresponses.Success(w, gensessions.SessionResponse{
+		Token: token,
+	})
 }
